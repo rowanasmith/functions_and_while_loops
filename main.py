f7bb6e857e4da9636f0555dbfef76290aaa59fb0
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-02-10 05:20:54 +0000

    defining a function

---
+++ main.py
@@ -0,0 +1,6 @@
+#functions are defined with def, then a name followed by parentheses. Then a colon. What the function will do will go below, and will be indented.
+
+def say_hi():
+  print("Hello World!")
+
+say_hi()
