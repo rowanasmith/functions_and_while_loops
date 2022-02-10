94b48e8e455be10684c465a1174ac86fc98b115d
rowanasmith <45500732+rowanasmith@users.noreply.github.com>
2022-02-10 05:27:29 +0000

    define while loops

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ def say_hi():
   print("Hello World!")
 
 say_hi()
+
+#while loops will repeat a task so long as the stated conditions are true. It is defined similarly to a for loop
+
+steps = 6
+while steps > 0:
+  print("Took a step")
+  steps -= 1
+
+#unlike for loops, while loops can go infinite!
